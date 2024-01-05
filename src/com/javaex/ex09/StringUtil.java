feb9ff4680f1f7e82fbaa965b03af14cb51f6b39
seed-yoo <lukdown10@naver.com>
2024-01-05 18:09:43 +0900

    .

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,10 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] strArray){
        
         //메소드 내용작성
-    	
+    	return strArray[0] + strArray[1] + strArray[2];
     }
 
 }
