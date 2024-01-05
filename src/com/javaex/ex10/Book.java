3e66ecbb8d0ccf14f7689965a51e9098713149ae
seed-yoo <lukdown10@naver.com>
2024-01-05 18:13:36 +0900

    수정중

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -2,10 +2,20 @@ package com.javaex.ex10;
 
 public class Book {
     
+	// 필드
     private int bookNo;
     private String title;
     private String author;
     private int stateCode;
+    
+    // 생성자
+	public Book(int bookNo, String title, String author) {
+		super();
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		this.stateCode = 1;
+	}
     
     
     
