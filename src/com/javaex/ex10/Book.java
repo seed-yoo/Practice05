ed388f09b378557e438a58ac0db94d7495cfce11
seed-yoo <lukdown10@naver.com>
2024-01-07 23:06:20 +0900

    연습문제(수정 완)

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,14 +1,18 @@
 package com.javaex.ex10;
 
 public class Book {
-    
+
 	// 필드
-    private int bookNo;
-    private String title;
-    private String author;
-    private int stateCode;
-    
-    // 생성자
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+	// 생성자
+	public Book() {
+		super();
+	}
+	
 	public Book(int bookNo, String title, String author) {
 		super();
 		this.bookNo = bookNo;
@@ -16,8 +20,58 @@ public class Book {
 		this.author = author;
 		this.stateCode = 1;
 	}
-    
-    
-    
-    
+
+
+	// 메소드 - getter/setter
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	// 메소드 - 일반
+	public void rent() {
+		this.stateCode = 0;
+		System.out.println(title + "이(가) 대여 됐습니다.");
+	}
+
+	public void print() {
+		if (stateCode == 0) {
+			System.out.println("대여 유무:대여중");
+		} else if (stateCode == 1) {
+			System.out.println("대여 유무:재고있음");
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return bookNo + " " + "책 제목:" + title + ", 작가:" + author;
+	}
+
 }
